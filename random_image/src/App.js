a894f4d2e4ef2848455c793e261b9ea8c620bd42
Malcomn <malcomn.j.mitchell@gmail.com>
2022-08-29 10:10:16 -0400

    fix wording

--- random_image/src/App.js
+++ random_image/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 /**
  * Using the Pokemon API, create a React app that displays a random pokemon from the show.
  *  1.) The app should display the character's name, front_default official-artwork, and the pokemon type(s).
- *  2.) The app should also display a button that when clicked, will fetch a new character random from the API and display it. 
+ *  2.) The app should also display a button that when clicked, will fetch a new random character from the API and display it. 
  *  3.) The app should show a loading status indicator, and display an error message if the API call fails.
  * */ 
 
